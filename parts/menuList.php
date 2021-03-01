b4a83ad66b0ccfee13f2c4243c858ca38740c330
shirai hiyuma <c.fhamc@gmail.com>
2021-03-01 21:26:14 +0900

    change to parts

---
+++ parts/menuList.php
@@ -0,0 +1,31 @@
+
+  <ol id="list" class="slide-in-animation">
+
+    <div class="line">MENU <i class="fas fa-list-ul"></i></div>
+    <li><i class="far fa-question-circle"></i> <a href="aboutsite.php">ABOUT</a></li>
+    <li><i class="far fa-address-card"></i> <a href="about.php">ADMINISTRATOR</a></li>
+    <li><i class="fab fa-pagelines" aria-hidden="true"></i> <a href="avocado.php">AVOCADO</a></li>
+    <li><i class="fas fa-book"></i> <a href="diary.php">DIARY</a></li>
+    <li><i class="fas fa-font"></i></a> <a href="english.php">ENGLISH</a>
+    </li>
+    <li><i class="fas fa-money-check-alt"></i> <a href="smt.php">INVESTMENT</a></li>
+    <li><i class="far fa-newspaper"></i> <a href="#news">NEWS</a></li>
+    <li><i class="fas fa-film"></i> <a href="movie.php">MOVIE </a></li>
+    <li><i class="fas fa-headphones-alt"></i> <a href="music.php">MUSIC
+        ※NEW</a></li>
+    <li><i class="fas fa-camera-retro"></i> <a href="gallery.php">PHOTO
+      </a></li>
+    <li><i class="fas fa-code"></i> <a href="#programming">PROGRAMMING</a>
+    </li>
+    <li><i class="far fa-sticky-note"></i> <a href="portfolio1/portfolio1.html">TEST PAGE</a></li>
+    <li><i class="fab fa-youtube"></i> <a href="video.php">VIDEO</a></li>
+    <li><i class="fas fa-laptop-code"></i> <a href="jspractice/index.php">WORK</a></li>
+    <li> <span><i class="far fa-edit"></i> <a href="questionnaire.php">アンケート</a></span></li>
+    <li><i class="fas fa-gamepad"></i> <a href="puyo2/www/puyoforlaptop.php">ゲームありあり</a></li>
+    <li><i class="fas fa-cat"></i> <a href="jspractice/nekochan.php">ねこちゃんSOS</a></li>
+    <li><i class="fas fa-paw"></i> <a href="jspractice/omikuji.php">ねこちゃんおみくじ</a></li>
+    <li><i class="fab fa-amazon"></i> <a href="#recommend">しらひゅー推し</a></li>
+    <!-- </div>
+                                   </div> -->
+  </ol>
+</div>
