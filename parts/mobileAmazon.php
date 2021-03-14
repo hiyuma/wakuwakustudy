89a3294c4655fd271794613b451eeabdc8d71933
shirai hiyuma <c.fhamc@gmail.com>
2021-03-14 13:30:23 +0900

    create mobileAmazon.php

---
+++ parts/mobileAmazon.php
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    amzn_assoc_ad_type = "link_enhancement_widget";
+    amzn_assoc_tracking_id = "hiyuma-22";
+    amzn_assoc_linkid = "a24734484bcc760b15805710f045b611";
+    amzn_assoc_placement = "";
+    amzn_assoc_marketplace = "amazon";
+    amzn_assoc_region = "JP";
+</script>
+<script src="//ws-fe.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&MarketPlace=JP"></script>
