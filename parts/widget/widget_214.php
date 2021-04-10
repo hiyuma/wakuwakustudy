424067944b3a5738bb1c093f3afcf58898b47a9f
shirai hiyuma <c.fhamc@gmail.com>
2021-04-10 12:11:56 +0900

    delete widget

--- parts/widget/widget_214.php
+++
@@ -1,3 +0,0 @@
-<div id="widget" class="214">
-<iframe src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=48&l=ur1&category=smarthome&banner=05VAFDAW5VSVWGD7DRG2&f=ifr&linkID=91b2fa93dfbdc4652a3fd4aa6690ae29&t=hiyuma-22&tracking_id=hiyuma-22" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
-</div>
--- parts/widget/widget_mujirushi.php
+++
@@ -1,3 +0,0 @@
-<div id="widget" class="mujirushi2">
-  <iframe src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=48&l=ur1&category=homekitchen&banner=1HY8QMW19KZAFF39AA02&f=ifr&linkID=290c135104bd6ccfad58ccfd2cad7e3b&t=hiyuma-22&tracking_id=hiyuma-22" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
-</div>
