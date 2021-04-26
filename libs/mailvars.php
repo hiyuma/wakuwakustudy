caf96d8d1f66045acf21d44812975c959183ea22
shirai hiyuma <c.fhamc@gmail.com>
2021-04-26 16:11:51 +0900

    create contact form

---
+++ libs/mailvars.php
@@ -0,0 +1,16 @@
+<?php
+//メールの宛先（To）の Email アドレス
+define('MAIL_TO', "info@developershirai.com");
+//メールの宛先（To）の名前  
+define('MAIL_TO_NAME', " ");
+//Cc の Email アドレス
+define('MAIL_CC', 'c.fhamc@gmail.com');
+//Cc の名前
+define('MAIL_CC_NAME', ' ');
+//Bcc の Email アドレス
+define('MAIL_BCC', ' ');
+//Return-Pathに指定するメールアドレス
+define('MAIL_RETURN_PATH', 'info@developershirai.com');
+//自動返信の返信先名前（自動返信を設定する場合）
+define('AUTO_REPLY_NAME', ' ');
+?>
