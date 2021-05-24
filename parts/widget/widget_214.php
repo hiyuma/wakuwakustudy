dd6c5dea270881427c8fec64e787b9296b6930e7
shirai hiyuma <c.fhamc@gmail.com>
2021-05-24 14:34:32 +0900

    delete widget

--- parts/widget/widget_214.php
+++ parts/widget/widget_214.php
@@ -1,5 +1,3 @@
 <div id="widget">
-<a href="https://px.a8.net/svt/ejp?a8mat=3BOCPF+9LWW0Q+50+2HH8I9" rel="nofollow">
-<img border="0" width="728" height="90" alt="" src="https://www24.a8.net/svt/bgt?aid=201010515581&wid=002&eno=01&mid=s00000000018015029000&mc=1"></a>
-<img border="0" width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=3BOCPF+9LWW0Q+50+2HH8I9" alt="">
+
 </div>
