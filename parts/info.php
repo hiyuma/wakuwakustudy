464c4ea8336553a40c4978a2834b3e5b256aeb14
shirai hiyuma <c.fhamc@gmail.com>
2022-01-23 12:27:02 +0900

    update movie

--- parts/info.php
+++ parts/info.php
@@ -3,6 +3,8 @@
 
 
   <div class="line"> INFO <i class="fas fa-info-circle"></i></div>
+  <li>2022年01月23日</li>
+  <div class="info-children"><span><a href="movie.php">MOVIE</a>（ものすごくうるさくて、ありえないほど近い）追加</span></div>
 
   <li>2022年01月21日</li>
   <div class="info-children"><span><a href="avocado.php">AVOCADO</a> 更新</span></div>
